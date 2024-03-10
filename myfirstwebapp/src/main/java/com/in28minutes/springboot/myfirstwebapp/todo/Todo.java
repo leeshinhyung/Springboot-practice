d7d007586a58395a8e8723d418fdf2d99d42217c
shl <shinhyung012@naver.com>
2024-03-10 20:15:38 +0900

    clear

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/Todo.java
@@ -1,14 +1,22 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity
 public class Todo {
+
+	public Todo(){
+
+	}
 
 //Database (MySQL)
 //Static List of Todos => Database (H2, MySQL)
-	
 	public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
 		super();
 		this.id = id;
@@ -17,7 +25,9 @@ public class Todo {
 		this.targetDate = targetDate;
 		this.done = done;
 	}
-	
+
+	@Id
+	@GeneratedValue
 	private int id;
 	private String username;
 
